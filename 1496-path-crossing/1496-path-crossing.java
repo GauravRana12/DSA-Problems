6f5c9b7904ca17f420542cb128cd15ac033f21de
Gaurav Rana <133829592+GauravRana12@users.noreply.github.com>
2023-12-23 14:20:25 +0530

    Time: 5 ms (42.95%), Space: 42.2 MB (5.51%) - LeetHub

---
+++ 1496-path-crossing/1496-path-crossing.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPathCrossing(String path) {
+        Set<String> set = new HashSet<>();
+        set.add("(0,0)");
+        int x = 0, y = 0;
+        for (int i = 0; i < path.length(); i++) {
+            if (path.charAt(i) == 'N') {
+                y++;
+            } else if (path.charAt(i) == 'S') {
+                y--;
+            } else if (path.charAt(i) == 'E') {
+                x++;
+            } else {
+                x--;
+            }
+            String s = "(" + x + "," + y + ")";
+            if (set.contains(s)) {
+                return true;
+            } else {
+                set.add(s);
+            }
+        }
+        return false;
+    }
+}
