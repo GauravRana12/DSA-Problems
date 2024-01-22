2f439c582901ff7e68ad5eb025e8941fc45ff3c7
Gaurav Rana <133829592+GauravRana12@users.noreply.github.com>
2024-01-22 10:43:21 +0530

    Time: 2 ms (90.94%), Space: 63.7 MB (9.97%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -15,23 +15,28 @@
  */
 class Solution {
     public int minDepth(TreeNode root) {
-        // Base case...
-        // If the subtree is empty i.e. root is NULL, return depth as 0...
-        if(root == null)  return 0;
-        // Initialize the depth of two subtrees...
-        int leftDepth = minDepth(root.left);
-        int rightDepth = minDepth(root.right);
-        // If the both subtrees are empty...
-        if(root.left == null && root.right == null)
-            return 1;
-        // If the left subtree is empty, return the depth of right subtree after adding 1 to it...
-        if(root.left == null)
-            return 1 + rightDepth;
-        // If the right subtree is empty, return the depth of left subtree after adding 1 to it...
-        if(root.right == null)
-            return 1 + leftDepth;
-        // When the two child function return its depth...
-        // Pick the minimum out of these two subtrees and return this value after adding 1 to it...
-        return Math.min(leftDepth, rightDepth) + 1;  
+         if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        int level = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            level++;
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = q.remove();
+                if (cur.left == null && cur.right == null) {
+                    return level;
+                }
+                if (cur.left != null) {
+                    q.add(cur.left);
+                }
+                if (cur.right != null) {
+                    q.add(cur.right);
+                }
+            }
+        }
+        return -1;
     }
 }
