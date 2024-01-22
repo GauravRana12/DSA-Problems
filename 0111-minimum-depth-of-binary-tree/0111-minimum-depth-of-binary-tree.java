bb87603d6fd304d7e90313cb58744d2fa6fba3a1
Gaurav Rana <133829592+GauravRana12@users.noreply.github.com>
2024-01-22 10:42:37 +0530

    Time: 4 ms (74.46%), Space: 62.9 MB (55.01%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -15,9 +15,23 @@
  */
 class Solution {
     public int minDepth(TreeNode root) {
-        if(root==null) return 0;
-        int right=minDepth(root.right);
-        int left=minDepth(root.left);
-        return (right==0 || left==0) ? left+right+1 :Math.min(right,left)+1;
+        // Base case...
+        // If the subtree is empty i.e. root is NULL, return depth as 0...
+        if(root == null)  return 0;
+        // Initialize the depth of two subtrees...
+        int leftDepth = minDepth(root.left);
+        int rightDepth = minDepth(root.right);
+        // If the both subtrees are empty...
+        if(root.left == null && root.right == null)
+            return 1;
+        // If the left subtree is empty, return the depth of right subtree after adding 1 to it...
+        if(root.left == null)
+            return 1 + rightDepth;
+        // If the right subtree is empty, return the depth of left subtree after adding 1 to it...
+        if(root.right == null)
+            return 1 + leftDepth;
+        // When the two child function return its depth...
+        // Pick the minimum out of these two subtrees and return this value after adding 1 to it...
+        return Math.min(leftDepth, rightDepth) + 1;  
     }
 }
