bfda95c9ff0bf79de6cf29cfc1c53b952b4009ee
Gaurav Rana <133829592+GauravRana12@users.noreply.github.com>
2024-01-23 11:45:22 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (53.73%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -1,15 +1,29 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        int numRows=rowIndex+1;
-         List<List<Integer>> allrows = new ArrayList<List<Integer>>();
-	ArrayList<Integer> row = new ArrayList<Integer>();
-	for(int i=0;i<numRows;i++)
-	{
-		row.add(0, 1);
-		for(int j=1;j<row.size()-1;j++)
-			row.set(j, row.get(j)+row.get(j+1));
-		allrows.add(new ArrayList<Integer>(row));
-	}
-	return allrows.get(rowIndex);
+        List<Integer> result=new ArrayList<>();
+        if(rowIndex==0){
+              result.add(1);
+              return result;
+        }
+        if(rowIndex==1){
+            result.add(1);
+            result.add(1);
+            return result;
+        }
+        long mul=(long) rowIndex;
+        long div=0;
+        long val=1;
+        while(div<=(long)rowIndex){
+            if(div==0){
+                result.add(1);
+                div++;
+                continue;
+            }
+            val=val*mul/div;
+            mul--;
+            div++;
+            result.add((int)val);
+        }
+        return result;
     }
 }
