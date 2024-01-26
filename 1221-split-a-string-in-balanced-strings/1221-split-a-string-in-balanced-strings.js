20cd2119c9cf4081239c45868204126833cd282b
Gaurav Rana <133829592+GauravRana12@users.noreply.github.com>
2024-01-26 12:03:26 +0530

    Time: 38 ms (98.72%), Space: 49.2 MB (5.26%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
@@ -4,21 +4,12 @@
  */
 
 var balancedStringSplit = function(s) {
-   let matches = 0;
-	let balance = 0;
-
-	for (let i = 0; i < s.length; i++) {
-
-		if (s[i] === "R") {
-			balance -= 1;
-		} else if (s[i] === "L") {
-			balance += 1;
-		}
-
-		if (balance === 0) {
-			matches += 1;
-		}
-	}
-
-	return matches;
+   let balance = 0
+    let result = 0
+    for (const i of s) {
+        if (i === "L") balance--;
+        else balance++;
+        if (balance === 0) result++
+    }
+    return result
 };
