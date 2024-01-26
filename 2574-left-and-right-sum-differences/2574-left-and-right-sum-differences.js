d17ecd39fa0a34c7edcd0232fff470caa1710fb7
Gaurav Rana <133829592+GauravRana12@users.noreply.github.com>
2024-01-26 10:35:01 +0530

    Time: 59 ms (87.53%), Space: 52 MB (6.17%) - LeetHub

---
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var leftRightDifference = function(nums) {
+      let leftSum = 0;
+    let rightSum = nums.reduce((sum,curr)=>sum += curr,0);
+
+    return nums.map((curr,i)=>{
+        //leftSum - (rightSum - curr)
+        rightSum -= curr;
+        let res = Math.abs(leftSum - rightSum );
+        leftSum += curr;
+        return res
+    });
+};
