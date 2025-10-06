d1f070d93935f43559eae6c8213ab581d7fd99b6
Gaurav Rana <133829592+GauravRana12@users.noreply.github.com>
2025-10-06 13:44:05 +0530

    Time: 0 ms (100%), Space: 56.9 MB (23.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,12 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
+   let map = new Map();
+     for(let i=0; i<nums.length; i++){
+        let complement = target - nums[i];
+        if(map.has(complement)){
+            return [map.get(complement),i]
         }
-    }
-    return nums;
+        map.set(nums[i],i)
+     }
 };
